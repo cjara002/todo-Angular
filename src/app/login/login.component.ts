f0a2c3d1a704757eec1fa60413f468b8631d9acd
Carlos Jara <cjjara002@gmail.com>
2025-01-27 10:35:30 -0800

    implmented hard coded validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ FormsModule],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  standalone: true,
 })
 export class LoginComponent {
-username = "TestUser"
+username = "in28minutes"
 password=''
+errorMessage = 'Invalid Credentials'
+invalidLogin = false
 
 handleLogin() {
-  console.log(this.username);
+  // console.log(this.username);
+  //Always need to use this key word
+  if(this.username === 'in28minutes' && this.password === 'dummy'){
+    this.invalidLogin = false;
+  } else{
+    this.invalidLogin = true;
+  }
 }
 }
