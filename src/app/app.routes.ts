782a852a2d038c92efdac07cf4ebd553c75fb4bf
Carlos Jara <cjjara002@gmail.com>
2025-03-04 17:06:06 -0800

    added log out component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListtodosComponent } from './listtodos/listtodos.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
     {path: "", component: LoginComponent},
     {path: "login", component: LoginComponent},
     {path: "welcome/:name", component: WelcomeComponent},
     {path:"todos", component: ListtodosComponent},
+    {path:"logout", component: LogoutComponent},
     {path: "**", component:ErrorComponent}
 ];
---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthenticationService } from '../hardcoded-authentication.service';
+
+@Component({
+  selector: 'app-logout',
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.css'
+})
+export class LogoutComponent  implements OnInit{
+
+  constructor (
+  private hardcodedAuthenticationService: HardcodedAuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    this.hardcodedAuthenticationService.logout();
+  }
+
+}
